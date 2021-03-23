cc5ca345f6e029e245c6618db483a7e81ca2ea3e
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-03-23 12:33:40 +0100

    Coords for shot

--- Battle-Ship-Game/Program.cs
+++ Battle-Ship-Game/Program.cs
@@ -4,6 +4,22 @@ namespace Battle_Ship_Game
 {
     class Program
     {
+
+        public struct Coords
+        {
+            public Coords(double x, double y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public double X { get; }
+            public double Y { get; }
+
+            public override string ToString() => $"({X}, {Y})";
+        }
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
