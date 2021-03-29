e53887f845aeded5029d127e8d8ad53fd6933bda
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-03-29 18:02:16 +0200

    moving classes

---
+++ Battle-Ship-Game/BattleShipGame.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace Battle_Ship_Game
+{
+    public class BattleShipGame
+    {
+        private int maxNumberOfAttempts = 10;
+
+
+        public static void AskForAShot()
+        {
+
+        }
+
+        public static void ValidAShot()
+        {
+
+        }
+
+        public static void CheckStatusShot()
+        {
+
+        }
+
+        public static void CheckNumberOfAttempts()
+        {
+
+        }
+    }
+}
