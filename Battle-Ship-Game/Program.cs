14812dad771dfceb2ad4e065f7a0c69ba35685d2
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-03-29 13:21:44 +0200

    updating gameboard

--- Battle-Ship-Game/Program.cs
+++ Battle-Ship-Game/Program.cs
@@ -4,26 +4,17 @@ namespace Battle_Ship_Game
 {
     class Program
     {
-
-        public struct Coords
-        {
-            public Coords(double x, double y)
-            {
-                X = x;
-                Y = y;
-            }
-
-            public double X { get; }
-            public double Y { get; }
-
-            public override string ToString() => $"({X}, {Y})";
-        }
-
+        
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello BattleShip");
         }
+
+       
+
+
+
 
     }    
 
---
+++ GameBoard.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Battle_Ship_Game
+{
+    public class GameBoard
+    {
+        
+
+
+        public static void printBoard()
+        {
+
+        }
+
+        public static void updateBoard()
+        {
+
+        }
+        public static void cleanBoard()
+        {
+
+        }
+
+        public static void PlaceShip()
+        {
+
+        }
+    }
+}
