cbe9aee48df376f73eade5e5aac42d1d7b195359
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-03-29 14:22:52 +0200

    game rules

--- Battle-Ship-Game/Program.cs
+++ Battle-Ship-Game/Program.cs
@@ -8,11 +8,40 @@ namespace Battle_Ship_Game
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello BattleShip");
+            PrintHelloMessage();
+            bool isGamePlaying = AskStartGame();
+
+            do{
+                
+            }while(isGamePlaying == true);
         }
 
        
+        public static void PrintHelloMessage(){
+            Console.WriteLine("** Welcome to The BattleShip Game! **");
+        }
 
+        public static bool AskStartGame(){
+            Console.WriteLine("Do you want to start playing? (Answer: 'yes' or 'no' )");
+            bool isValidAnswer = false;
+            do{
+
+                string input = Console.ReadLine();
+                if(input == "yes")
+                {   
+                    isValidAnswer = true;
+                    return true;
+                }else if(input == "no")
+                {
+                    isValidAnswer = true;
+                    return false;
+                } else {
+                    Console.WriteLine("Please write a valid answer: 'yes' or 'no'.");
+                    isValidAnswer = false;
+                }
+            } while(isValidAnswer == false); 
+            return false;
+        }
 
 
 
