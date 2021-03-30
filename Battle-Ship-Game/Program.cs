2269c004cfecf9b7b7f827fda00948c5ab00c3cb
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-03-30 19:14:43 +0200

    cleaning and starting the game

--- Battle-Ship-Game/Program.cs
+++ Battle-Ship-Game/Program.cs
@@ -8,9 +8,44 @@ namespace Battle_Ship_Game
 
         static void Main(string[] args)
         {
-           Console.WriteLine("** BattleShip Game **");
+            
+            //PrintWelcomeMessage();
+
+            if(AskForNewGame()){
+                Console.WriteLine("Juguemos!!");
+            }
 
             
+        }
+
+        public static bool AskForNewGame()
+        {
+            
+
+            bool isGameActive = false;
+          
+            PrintWelcomeMessage();
+
+            Console.WriteLine("Do you want to play BattleShip Game?");
+            Console.WriteLine("Please type 'yes' or 'no'.");
+
+            string input = Console.ReadLine();
+            if(input == "no")
+            {
+                isGameActive = false;
+            } else if(input == "yes"){
+                isGameActive = true;
+            }
+            
+            return isGameActive;
+        }
+
+        public static void PrintWelcomeMessage()
+        {
+            Console.Clear();
+            Console.WriteLine(" ** BattleShip Game ** ");
+            Console.WriteLine("=======================");
+
         }
 
        
